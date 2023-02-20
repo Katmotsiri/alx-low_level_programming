7f27612d6902af57fc5275be8f4f02550077de2a
Katmotsiri <katlehomotsiri02@gmail.com>
2023-02-19 16:27:30 -0800

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -3,12 +3,12 @@
 /**
  * main - Entry point
  *
- * Description:'A c program that prints a line in the standard error'
+ * Description: 'A c program that prints a line in the standard error'
  *
- * Return 1 (Success)
+ * Return: 1 (Success)
  */
 int main(void)
 {
-	fputs("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);
+fputs("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);
 return (1);
 }
