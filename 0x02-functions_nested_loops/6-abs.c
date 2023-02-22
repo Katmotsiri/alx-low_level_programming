2829f6cebe800acfb32504257b04113abd6d96a8
Katmotsiri <katlehomotsiri02@gmail.com>
2023-02-22 11:26:22 -0800

    absolute

--- 0x02-functions_nested_loops/6-abs.c
+++ 0x02-functions_nested_loops/6-abs.c
@@ -2,19 +2,16 @@
 
 /**
  * _abs - Computes the abdolute value of an interger.
- * @c: The number to be computed.
- * Return: Absolute value of number or zero
+ * @n: The int to check
+ * Return: Absolute value of int
  */
 
-int _abs(int c)
+int _abs(int n)
 
 {
-	if (c < 0)
+	if (n >= 0)
 	{
-	int abs_val;
-
-	abs_val = c + -1;
-	return (abs_val);
+	return (n);
 	}
-	return (c);
+	return (-n);
 }
