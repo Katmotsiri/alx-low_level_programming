a1695a12b65f4121cb511b9904d7aab79cced9ee
Katmotsiri <katlehomotsiri02@gmail.com>
2023-02-27 10:47:33 -0800

    puts2

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -5,7 +5,7 @@
  * @str: input
  * Return: print
  */
-void puts2(char *str);
+void puts2(char *str)
 {
 	int longi = 0;
 	int t = 0;
@@ -20,7 +20,7 @@ void puts2(char *str);
 	t = longi - 1;
 	for (o = 0; o <= t; o++)
 	{
-		if (o % 2 == 0)
+	if (o % 2 == 0)
 	}
 	_putchar(str[o]);
 	{
